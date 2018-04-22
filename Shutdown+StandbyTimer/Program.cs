ded060dc1a104df514f327ffbfd1c56174bf65a1
padarc <padarc@gmx.de>
2018-04-22 13:34:38 +0200

    ll

---
+++ Shutdown+StandbyTimer/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shutdown_StandbyTimer
+{
+    class Program
+    {
+        static int Choice;
+        static int MinuteInput;
+
+        //Import for standby
+        [DllImport("Powrprof.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern bool SetSuspendState(bool hiberate, bool forceCritical, bool disableWakeEvent);
+        //
+
+        static void Main(string[] args)
+        {
+            SetWindowSize(40, 10);
+            ClearConsoleAndDisplayHeader();
+            ReadChoice();
+            ReadMinuteInput();
+            StartTimer();
+        }
+
+        static void SetWindowSize(int Width, int Height)
+        {
+            Console.SetWindowSize(Width, Height);
+            Console.SetBufferSize(Width, Height);
+        }
+
+        static void ClearConsoleAndDisplayHeader()
+        {
+            Console.Clear();
+            Console.WriteLine("#########################");
+            Console.WriteLine("# Shutdown+StandbyTimer #");
+            Console.WriteLine("#########################");
+            Console.WriteLine();
+        }
+
+        static void ReadChoice()
+        {
+            ClearConsoleAndDisplayHeader();
+            Console.Write("Type 1 for Standby, 2 for Shutdown: ");
+            Choice = Convert.ToInt32(System.Console.ReadLine());
+        }
+
+        static void ReadMinuteInput()
+        {
+            ClearConsoleAndDisplayHeader();
+            if (Choice == 1)
+            {
+                Console.Write("How many minutes until standby?: ");
+                MinuteInput = Convert.ToInt32(System.Console.ReadLine());
+            }
+
+            else if (Choice == 2)
+            {
+                Console.Write("How many minutes until shutdown?: ");
+                MinuteInput = Convert.ToInt32(System.Console.ReadLine());
+            }
+
+            else
+            {
+                ClearConsoleAndDisplayHeader();
+                Console.WriteLine("Wrong input, please try again!");
+                Main(null);
+            }
+        }
+
+        public static void StartTimer()
+        {
+            int MsRemaining =  MinuteInput * 60000;
+            ClearConsoleAndDisplayHeader();
+            Console.Write("Minutes remaining: " + MsRemaining / 60000);
+            while (MsRemaining > 0)
+            {
+                System.Threading.Thread.Sleep(60000);
+                MsRemaining = MsRemaining - 60000;
+                ClearConsoleAndDisplayHeader();
+                Console.Write("Minutes remaining: " + MsRemaining / 60000);
+            }
+
+            if (Choice == 1)
+            {
+                ClearConsoleAndDisplayHeader();
+                InitializeStandby();
+            }
+
+            else if (Choice == 2)
+            {
+                ClearConsoleAndDisplayHeader();
+                InitializeShutdown();
+            }
+        }
+
+        static void InitializeStandby()
+        {
+            Console.WriteLine("Initializing Standby...");
+            SetSuspendState(false, true, true);
+            Console.WriteLine("All done, press any key to exit!");
+            String s = Console.ReadLine();
+
+        }
+
+        static void InitializeShutdown()
+        {
+            Console.WriteLine("Initializing Shutdown...");
+            Process.Start("shutdown", "/s /t 0");
+            Console.WriteLine("All done, press any key to exit!");
+            String s = Console.ReadLine();
+        }
+    }
+}
---
+++ Shutdown+StandbyTimer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Shutdown+StandbyTimer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Shutdown+StandbyTimer")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("19065161-5bdb-4817-97a1-604a39717c5c")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
